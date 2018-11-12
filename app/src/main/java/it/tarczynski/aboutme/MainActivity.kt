0643aefce86939789c039589452846ef7905b245
Tomasz Tarczyński <tarczynskitomek@gmail.com>
2018-11-12 10:51:25 +0100

    Databinding step

--- app/src/main/java/it/tarczynski/aboutme/MainActivity.kt
+++ app/src/main/java/it/tarczynski/aboutme/MainActivity.kt
@@ -1,12 +1,39 @@
 package it.tarczynski.aboutme
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Context
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import it.tarczynski.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+    private val myName = MyName("Tomek Tarczyński")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
+        binding.doneButton.setOnClickListener {
+            addNickname(it as Button)
+        }
+    }
+
+    private fun addNickname(doneButton: Button) {
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            doneButton.visibility = View.GONE
+            nicknameEdit.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(doneButton.windowToken, 0)
     }
 }
---
+++ app/src/main/java/it/tarczynski/aboutme/MyName.kt
@@ -0,0 +1,3 @@
+package it.tarczynski.aboutme
+
+data class MyName(var name: String = "", var nickname: String = "")
